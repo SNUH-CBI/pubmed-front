440618da9a4baec018243227d8213a841047887c
yecnj <yecnj.office@gmail.com>
2019-10-10 17:29:14 +0900

    barchart interaction & filter

--- src/component/barchart.js
+++ src/component/barchart.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import Chart from 'chart.js';
 
+import FilterComponent from '../component/filter'
+
 const red = "rgb(255, 99, 132)"
-const transRed = Chart.helpers.color("rgb(255, 99, 132)").alpha(0.4).rgbString()
+const transRed = Chart.helpers.color(red).alpha(0.4).rgbString()
+
+const blue = "rgb(132, 100, 255)"
+const transBlue = Chart.helpers.color(blue).alpha(0.4).rgbString()
+
+function setBgColor(_chart, _color, _index=-1){
+  if (_index === -1)
+    _chart.data.datasets[0].backgroundColor = _color
+  else
+    _chart.data.datasets[0].backgroundColor[_index] = _color
+}
 
 export default class BarChartComponent extends React.Component{
   chartRef = React.createRef();
@@ -10,7 +22,8 @@ export default class BarChartComponent extends React.Component{
     length: this.props.data.length,
     labels: this.props.data.map((value, key) => {return value.key}),
     values: this.props.data.map((value, key) => {return value.doc_count}),
-    clicked: false
+    click: { on: false, index: -1, year: -1 },
+    totalPub: 0
   } 
   componentDidMount() {
     const myChartRef = this.chartRef.current.getContext("2d")
@@ -20,9 +33,8 @@ export default class BarChartComponent extends React.Component{
         labels: this.state.labels,
         datasets: [
           {
-            label: "publication: ",
-            backgroundColor: Array(this.state.length).fill(transRed),
-				    borderColor: red,
+            backgroundColor: Array(this.state.length).fill(blue),
+				    borderColor: blue,
             data: this.state.values
           }
         ]
@@ -32,19 +44,55 @@ export default class BarChartComponent extends React.Component{
         legend: { display: false },
         title: { display: false },
         onClick: (e, item) => {
-          if (!this.state.clicked && item[0]){
-            chart.data.datasets[0].backgroundColor = Array(this.state.length).fill(transRed)
-            this.setState({ clicked: true })
-            chart.data.datasets[0].backgroundColor[item[0]._index] = red
+          if (!this.state.click.on && item[0]){
+            let [index, year] = [item[0]._index, item[0]._view.label]
+
+            setBgColor(chart, Array(this.state.length).fill(transBlue))
+            this.setState({ click: {
+                on: true,
+                index: index,
+                year: year,
+              },
+              totalPub: this.state.values[index]
+            })
+            setBgColor(chart, blue, index)
+
             chart.update()
-          } else if (this.state.clicked) {
-            this.setState({ clicked: false })
+          }
+          else if (this.state.click.on) {
+            this.setState({ click: { on: false } })
           }
         },
         onHover: (e, item) => {
-          if (this.state.clicked && item[0]){
-            chart.data.datasets[0].backgroundColor[item[0]._index] = red
+          if (this.state.click.on && item[0]){
+            let _index = item[0]._index
+            let [from, to] = [this.state.click.index, _index].sort()
+            let total = 0
+
+            setBgColor(chart, Array(this.state.length).fill(transBlue))
+            for (let i = from; i <= to; i++){
+              total += this.state.values[i]
+              setBgColor(chart, blue, i)
+            }
+            this.setState({ totalPub: total })
+
             chart.update()
+          }
+        },
+        tooltips: {
+          callbacks: {
+            title: (tti, data) => {
+              if (this.state.click.on)
+                return [this.state.click.year, tti[0].label].sort().join(" ~ ")
+              else
+                return tti[0].label
+            },
+            label: (tti, data) => {
+              if (this.state.click.on)
+                return "Pub. number : " + this.state.totalPub
+              else
+                return "Pub. number: " + tti.yLabel
+            }
           }
         }
       }
@@ -54,6 +102,7 @@ export default class BarChartComponent extends React.Component{
     return (
       <div className={this.props.className}>
         <canvas id="barChart" ref={ this.chartRef } />
+        <FilterComponent />
       </div>
     )
   } 
