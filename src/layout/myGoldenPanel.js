d439b33b1ebaeeba50d426c05bcd20e1e361f120
yecnj <yecnj.office@gmail.com>
2019-11-05 10:39:36 +0900

    remove golden layout

--- src/layout/myGoldenPanel.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export class MyGoldenPanel extends React.Component {
-  render() {
-    return (
-      <div id={this.props.id} className={this.props.className}> </div>
-    );
-  }
-}
